3657bbb62320c945ba886489a9941908139ed720
pluizer <pluizer@freeshell.de>
2014-11-24 23:49:56 +0000

    improved and renamed window functions

--- src/window.scm
+++ src/window.scm
@@ -5,29 +5,39 @@
      (prefix gl-utils gl::)
      srfi-18)
 
-(define (window:create w h title fullscreen? iter)
+(define %window-should-close? #f)
+(define (coati:close)
+  (set! %window-should-close? #t))
+
+;; Starts coati and starts the game loop with the function
+;; '''returned''' by ''loop-func''. Getting the loop function
+;; in this way makes it more convinient to init objects that
+;; depend on Coati to be started first.
+(define (coati:start w h title fullscreen? loop-func)
   (fw::init)
   (fw::with-window (w h 
 		      title
 		      fullscreen?: fullscreen?
 		      resizable:  #f
-		      context-version-major: 3
-		      context-version-minor: 3)
-		   (fw::make-context-current (fw::window))
+)
+;		   (fw::make-context-current (fw::window))
 		   (gl::init)
 		   (gl::enable gl::+texture-2d+)
 		   (gl::enable gl::+blend+)
 		   (gl::disable gl::+depth-test+)
+		   (fw::swap-interval 0)
 		   (gl::check-error)
-		   (let loop () 
-		     (iter)
-		     (poll-events!)
-		     (fw::swap-buffers (fw::window))
-		     (fw::poll-events)
-		     (if (and (iter)
-			      (not (fw::window-should-close (fw::window))))
-			 (loop)
-			 (begin
-			   (free-all-resources)
-			   (fw::set-window-should-close (fw::window) #t))))))
+		   (let ((iter (loop-func)))
+		    (let loop () 
+		      (iter)
+		      (poll-events!)
+		      (fw::swap-buffers (fw::window))
+		      (fw::poll-events)
+		      (if (and (iter)
+			       (not %window-should-close?)
+			       (not (fw::window-should-close (fw::window))))
+			  (loop)
+			  (begin
+			    (free-all-resources)
+			    (fw::set-window-should-close (fw::window) #t)))))))
 
