c93da66bdfa2449cdd7f7e0f4251a6644eafc38a
pluizer <pluizer@freeshell.de>
2014-12-06 00:21:35 +0000

    texture:fullscreen-renderer

--- src/texture.scm
+++ src/texture.scm
@@ -101,7 +101,21 @@
 	(sprite-batcher (sprite-batcher:create)))
     (sprite-batcher:push! sprite-batcher sprite (identity-matrix))
     (lambda (projection view)
-      (sprite-batcher:render sprite-batcher projection view))))
+      (with-texture/proc texture
+	(lambda ()
+	 (sprite-batcher:render sprite-batcher projection view))))))
+
+;; Returns a function that renders a texture fullscreen.
+(define (texture:fullscreen-renderer texture
+				     #!optional (rect (rect:create 0 1 1 0)))
+  (let ((renderer (texture:renderer texture rect))
+	(projection (identity-matrix))
+	(view (f32vector 2 0 0 0
+			 0 2 0 0
+			 0 0 1 0
+			 -1 -1 0 1)))
+    (lambda ()
+      (renderer projection view))))
 
 (define (with-texture/proc texture thunk)
   (gl::with-texture gl::+texture-2d+ (texture:texture-id texture)
