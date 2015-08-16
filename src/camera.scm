60a41aa60469dbb972bc314dd8f06f3987e314a0
Richard <pluizer@Richards-MacBook-Pro.local>
2015-08-16 23:37:36 +0200

    default camera to resolution size

--- src/camera.scm
+++ src/camera.scm
@@ -5,10 +5,10 @@
 (define-record camera
   pos zoom width height projection view)
 
-(define (camera:create pos zoom #!optional (width 1) (height 1))
-  (make-camera pos zoom width height
-               (ortho-matrix (* zoom width)
-                             (* zoom height))
+(define (camera:create pos zoom #!optional (size (window:size)))
+  (make-camera pos zoom (vect:x size) (vect:y size)
+               (ortho-matrix (* zoom (vect:x size))
+                             (* zoom (vect:y size)))
                (look-at-matrix pos pos 1)))
 
 (define camera:pos camera-pos)
@@ -18,9 +18,11 @@
 (define camera:projection camera-projection)
 (define camera:view camera-view)
 
-;; TODO FIXME, change when resolution changes.
-(define %camera-stack (list (camera:create (zero-vect) 1 1 1)))
-(define (current-camera) (car %camera-stack))
+(define %camera-stack (list))
+(define (current-camera) (if (null? %camera-stack)
+                             ;; Default current camera
+                             (camera:create (zero-vect) 1)
+                             (car %camera-stack)))
 
 
 (define (with-camera/proc camera thunk)
