f59ae66039632e09a66c9b75292976ef81cb62cf
Pluizer <pluizer@freeshell.de>
2015-08-17 23:44:46 +0200

    reverted back to sdl 1.2

--- src/window.scm
+++ src/window.scm
@@ -3,9 +3,7 @@
 
 (use (prefix opengl-glew gl::)
      (prefix gl-utils gl::)
-     sdl2
-     sdl2-raw
-     sdl2-types
+     sdl-base
      srfi-18)
 
 (define %window-should-close? #f)
@@ -15,12 +13,10 @@
 
 (define (window:size) %window-size)
 
-(define %current-window 0)
-
 (define (game-loop iter-func prev-ret)
   (poll-input-events)
   (poll-events!)
-  (SDL_GL_SwapWindow %current-window)
+  (sdl-gl-swap-buffers)
   (let ((ret (apply iter-func (if (list? prev-ret) prev-ret
                                   (list prev-ret)))))
     (when (and ret (not %window-should-close?))
@@ -42,28 +38,21 @@
 ;;                              (list (update-position position) 'walking)))
 ;;                        (list (vect:create 10 20) 'walking))))
 (define (coati:start w h title fullscreen? game)
-  (unless (sdl-init)
+  (unless (sdl-init SDL_INIT_EVERYTHING)
     (error "Could not initialise SDL." (sdl-get-error)))
-  ;; (sdl-gl-set-attribute SDL_GL_DOUBLEBUFFER 1)
-  ;; (sdl-gl-set-attribute SDL_GL_SWAP_CONTROL 1)
-  (SDL_GL_SetAttribute SDL_GL_CONTEXT_MAJOR_VERSION 2)
-  (SDL_GL_SetAttribute SDL_GL_CONTEXT_MINOR_VERSION 1)
-  (let ((window (sdl-create-window title 0 0 w h #:opengl #:shown #:input-grabbed
-                                   )))
-    (unless window
+  (sdl-gl-set-attribute SDL_GL_DOUBLEBUFFER 1)
+  (sdl-gl-set-attribute SDL_GL_SWAP_CONTROL 1)
+  (let ((surface (sdl-set-video-mode w h 32
+                                     (bitwise-ior (if fullscreen? SDL_FULLSCREEN 0)
+                                                  SDL_OPENGL))))
+    (unless surface
       (error (sprintf "Could not set video mode (~sx~s:~s):" w h 32) (sdl-get-error)))
-    (let ((context (SDL_GL_CreateContext window)))
-      (unless context
-        (error "No GL context"))
-      (SDL_GL_MakeCurrent window context)
-      (set! %current-window window)))
-  
-  (set! %window-size (vect:create w h))
-  (gl::init)
-  (gl::enable gl::+texture-2d+)
-  (gl::enable gl::+blend+)
-  (gl::disable gl::+depth-test+)
-  (gl::check-error)
-  (call-with-values
-      (lambda () (game))
-    (lambda (f #!optional (a (list))) (game-loop f a))))
+    (set! %window-size (vect:create w h))
+    (gl::init)
+    (gl::enable gl::+texture-2d+)
+    (gl::enable gl::+blend+)
+    (gl::disable gl::+depth-test+)
+    (gl::check-error)
+    (call-with-values
+        (lambda () (game))
+        (lambda (f #!optional (a (list))) (game-loop f a)))))
