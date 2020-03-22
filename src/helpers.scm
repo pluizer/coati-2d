2c07f4c72237c2022d1e1e69d82e6f28e3d0a74c
Richard van Roy <richard@vanroy.io>
2020-03-22 10:53:21 +0100

    removed files that are not needed anymore for Chicken 5

--- src/helpers.scm
+++
@@ -1,7 +0,0 @@
-;; Helper syntax
-
-;; Applies func to value, or if func is #f returns just the value.
-(define-syntax maybe
-  (syntax-rules ()
-      ((_ func value)
-       (if func (func value) value))))
